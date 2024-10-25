a057ae2d308f4e7b461cc2819974e92c6cc4b6fe
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-10-25 17:50:35 +0600

    Fixed run_at column defaults to current time

--- db/driver/gormdriver/model.go
+++ db/driver/gormdriver/model.go
@@ -10,7 +10,7 @@ type Model struct {
 	ID      uint       `json:"id" gorm:"primaryKey"`
 	Name    string     `json:"name" gorm:"type:varchar(250);uniqueIndex;not null;"`
 	Payload string     `json:"payload" gorm:"type:text;not null;"`
-	RunAt   *time.Time `json:"run_at" gorm:"type:timestamp;"`
+	RunAt   *time.Time `json:"run_at" gorm:"type:timestamp;default null;"`
 	Cron    *string    `json:"cron" gorm:"type:varchar(150);"`
 
 	CreatedAt time.Time `json:"created_at" gorm:"type:datetime"`
