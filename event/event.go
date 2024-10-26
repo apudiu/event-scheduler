6b4e14a1339c0df57c14fced6123a4c21a36d439
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-10-26 15:56:50 +0600

    Using interface as event payload

--- event/event.go
+++ event/event.go
@@ -1,13 +1,15 @@
 package event
 
+import "github.com/apudiu/event-scheduler/event/payload"
+
 type SchedulerEvent interface {
 	GetEvent() *Event
 }
 
 // Event structure
 type Event struct {
-	ID      uint    `json:"id"`
-	Name    string  `json:"name"`
-	Payload string  `json:"payload"`
-	Cron    *string `json:"cron"`
+	ID      uint                        `json:"id"`
+	Name    string                      `json:"name"`
+	Payload payload.TransferablePayload `json:"payload"`
+	Cron    *string                     `json:"cron"`
 }
