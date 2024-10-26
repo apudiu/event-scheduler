c176d4002881da494a2c59e76f5e967702c4db12
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-10-26 15:47:05 +0600

    Changed tables payload column to binary

--- db/driver/gormdriver/model.go
+++ db/driver/gormdriver/model.go
@@ -2,6 +2,7 @@ package gormdriver
 
 import (
 	"github.com/apudiu/event-scheduler/event"
+	"github.com/apudiu/event-scheduler/event/payload"
 	"time"
 )
 
@@ -9,7 +10,7 @@ import (
 type Model struct {
 	ID      uint       `json:"id" gorm:"primaryKey"`
 	Name    string     `json:"name" gorm:"type:varchar(250);index;not null;"`
-	Payload string     `json:"payload" gorm:"type:text;not null;"`
+	Payload []byte     `json:"payload" gorm:"type:varbinary(500);not null;"`
 	RunAt   *time.Time `json:"run_at" gorm:"type:datetime;"`
 	Cron    *string    `json:"cron" gorm:"type:varchar(150);"`
 
@@ -25,7 +26,7 @@ func (m *Model) GetEvent() *event.Event {
 	return &event.Event{
 		ID:      m.ID,
 		Name:    m.Name,
-		Payload: m.Payload,
+		Payload: payload.NewEncodedGobPayload(m.Payload),
 		Cron:    m.Cron,
 	}
 }
