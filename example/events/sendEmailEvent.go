9aa648eb3ef1f539e807f73bc818879d1d3ab560
Mohammad A. Apu <apuatcfbd@gmail.com>
2024-10-26 16:45:38 +0600

    Added comments

--- example/events/sendEmailEvent.go
+++ example/events/sendEmailEvent.go
@@ -7,9 +7,16 @@ import (
 	"time"
 )
 
+// receive whatever you need in args
+
 func SendEmailEvent(email string, content string) {
+
+	// prepare payload with provided payload.GobPayload
+	// or create one yourself by implementing payload.TransferablePayload interface
 	p := payload.NewGobPayload(
 		fmt.Sprintf("Send email to %s with content %s", email, content),
 	)
+
+	// send the event using the payload
 	bootstrap.SCD.Schedule("SendEmail", p, time.Now().Add(time.Second*5))
 }
